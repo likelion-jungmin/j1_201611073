f70d566a94465e8e73bc1a9288fbb23a134c512b
201611073_박정민 <pmin022@naver.com>
2017-05-22 01:59:25 +0900

    Delete PizzaIngredientFactory.java

--- factory/pizzaaf/small/PizzaIngredientFactory.java
+++
@@ -1,7 +0,0 @@
-package headfirst.factory.pizzaaf.small;
-
-public interface PizzaIngredientFactory {
-  public Dough createDough();
-  public Cheese createCheese();
-  public Potato createPotato();
-}
